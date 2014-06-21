3b18cf8331f147a85120d5fb1983bbed4b92fecd
Mike Vianueva <vianuevm@umich.edu>
2014-06-20 22:28:36 -0400

    Initial Commit

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int x = 0;
+int main() {
+   int x = 4/4;
+     int r =  2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2
+    if(2+2 = 4) {
+        cout << "Hello World!" << endl;
+         cout << "Hello World!" << endl;
+    }
+    return 0;
+}
