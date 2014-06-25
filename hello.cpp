2dbc3af76cf9355a09c0966c5459b655bf24f5ee
Mike Vianueva <vianuevm@umich.edu>
2014-06-25 18:07:56 -0400

    Updated parser.py
    Changed name to roger to avoid confusion.
    
    Made several changes after debugging.

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
 int x = 0;
-int main() {
-   int x = 4/4;
-     int r =  2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2
-    if(2+2 = 4) {
+
+int prototype(int one, void two);
+
+int main(){
+    int x = 4 / 4;
+    //int r =  2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2
+     if(2 + 2 = 4)
+    {
         cout << "Hello World!" << endl;
-         cout << "Hello World!" << endl;
+        cout << "Hello World!" << endl;
     }
     return 0;
+}
+
+int prototype(int one, void two)
+{
+    int x = 2;
+    return x;
 }
--- roger.py
+++ roger.py
@@ -13,43 +13,45 @@ import string
 import sys
 import unicodedata
 
-
-
- #TODO This is on line 1695 cpplint
- ## Remember initial indentation level for this class.  Using raw_lines here
- #   # instead of elided to account for leading comments.
- #   initial_indent = Match(r'^( *)\S', clean_lines.raw_lines[linenum])
- #   if initial_indent:
- #     self.class_indent = len(initial_indent.group(1))
- #   else:
- #     self.class_indent = 0
-
+#TODO: Find this data a non-global home
+#This is global/hard-coded for now till it gets a database home.
 list_of_errors = {}
-list_of_errors["OPERATOR_SPACE_ERROR"] = "Incorrect spacing around operators"
-list_of_errors["INDENTATION_ERROR"] = "Incorrect indentation.  Check to make sure you are four spaces in from previous code block."
-list_of_errors["COMMAND_ERROR"] = "There should only be one command (statement) on each line.  Ever."
-list_of_errors["IF_ELSE_ERROR"] = "Every If-Else statement should have brackets, regardless."
-list_of_errors["GLOBAL_VARIABLE"] = "You should never, ever, ever... wait for it... ever have a non-const global variable."
-list_of_errors["FUNCTION_LENGTH_ERROR"] = "Your function should not be this long.  Break it up into separate functions."
-list_of_errors["LINE_WIDTH"] = "You exceeded 80 chars on a line.  This needs to be reformatted."
-list_of_errors["BOOL_VALUE"] = "You need to return true or false.  Not 1 or 0."
-list_of_errors["MAGIC_NUMBER"] = "Every number should be stored in a variable, not used as a literal."
-list_of_errors["BRACES_ERROR"] = "Your braces should be egyption style or block style.  You have some kind of formatting error."
-list_of_errors["SPACING_ERROR"] = "Use tabs or spaces, not both."
-list_of_errors["UNNECESSARY_BREAK"] = "Breaks should ONLY be used in switch statements.  Fix your logic."
-list_of_errors["GOTO"] = "Never use the goto function."
+list_of_errors["OPERATOR_SPACE_ERROR"] = \
+    "Incorrect spacing around operators"
+list_of_errors["INDENTATION_ERROR"] = \
+    "Incorrect indentation.  Check to make sure you are four spaces in from previous code block."
+list_of_errors["COMMAND_ERROR"] = \
+    "There should only be one command (statement) on each line.  Ever."
+list_of_errors["IF_ELSE_ERROR"] = \
+    "Every If-Else statement should have brackets, regardless."
+list_of_errors["GLOBAL_VARIABLE"] = \
+    "You should never, ever, ever... wait for it... ever have a non-const global variable."
+list_of_errors["FUNCTION_LENGTH_ERROR"] = \
+    "Your function should not be this long.  Break it up into separate functions."
+list_of_errors["LINE_WIDTH"] = \
+    "You exceeded 80 chars on a line.  This needs to be reformatted."
+list_of_errors["BOOL_VALUE"] = \
+    "You need to return true or false.  Not 1 or 0."
+list_of_errors["MAGIC_NUMBER"] = \
+    "Every number should be stored in a variable, not used as a literal."
+list_of_errors["BRACES_ERROR"] = \
+    "Your braces should be egyption style or block style.  You have some kind of formatting error."
+list_of_errors["SPACING_ERROR"] = \
+    "Use tabs or spaces, not both."
+list_of_errors["UNNECESSARY_BREAK"] = \
+    "Breaks should ONLY be used in switch statements.  Fix your logic."
+list_of_errors["GOTO"] = \
+    "Never use the goto function."
 
 
 class Default_Filters():
-
     def __init__(self):
         self.goto = True
         self.globals = True
         self.breaks = True
 
+#Todo: Define filters through command line arguments
 DEFAULT_FILTERS = Default_Filters()
-OUTSIDE_MAIN = True
-
 
 class StyleError():
     def __init__(self):
@@ -57,7 +59,6 @@ class StyleError():
         self.label = ""
         self.points_worth = 0
 
-    #TODO: Make this like one line of code with hash table!?
     def __init__(self, points, label, line_num):
         self.setPointsWorth(points)
         self.setLineNum(line_num)
@@ -198,7 +199,7 @@ def validReturn(filename, clean_lines, line, line_num, rubric):
         list_of_line = current_line.split(' ')
         if len(list_of_line) > 2:
             #TODO Figure out what you want to take off with a more than one statement on return line
-            print "do the to do"
+            pass
         else:
             current_line = current_line[6:].strip()
             if(current_line.isdigit()):
@@ -232,7 +233,6 @@ def operatorSpacing(filename, clean_lines, line, line_num, operator_space_tracke
         not checkOperatorRegEx(code, '\/') or \
         not checkOperatorRegEx(code, '\%') or \
         not checkOperatorRegEx(code, '\*'):
-
         rubric.incrementErrorCount("OPERATOR_SPACE_ERROR", line_num)
 
     else:
@@ -261,7 +261,7 @@ def nonConstGlobal(filename, clean_lines, line_num, rubric):
         rubric.setInsideMain()
 
     if(rubric.isOutsideMain()):
-        function = re.search(r'(\w(\w|::|\*|\&|\s)*)\(', code)
+        function = isFunction(code)
         variable = re.search(r'^\s*(int|string|char|bool)\s+', code)
         if not function and variable:
             rubric.incrementErrorCount("GLOBAL_VARIABLE", line_num)
@@ -270,13 +270,13 @@ def nonConstGlobal(filename, clean_lines, line_num, rubric):
 def braceConsistency(clean_lines, line, line_num, rubric):
     code = clean_lines.lines[line_num]
     stripped_code = code.strip()
-    function = re.search(r'(\w(\w|::|\*|\&|\s)*)\(', code)
+    function = isFunction(code)
     if_statement = re.search(r'^if\s*\(\s*', stripped_code)
     else_if_statement = re.search(r'^else\s*\(', code)
     else_statement = re.search(r'^else\s+', code)
     switch_statement = re.search(r'^switch\s*\(', stripped_code)
 
-    if function or else_if_statement or else_statement or switch_statement:
+    if function or if_statement or else_statement or switch_statement:
 
         if function and clean_lines.lines[line_num + 1].find('{') != -1 or\
             else_if_statement and clean_lines.lines[line_num + 1].find('{') != -1 or\
@@ -292,28 +292,33 @@ def braceConsistency(clean_lines, line, line_num, rubric):
                 if_statement and code.find('{') != -1:
 
             rubric.isEgyptian(True)
-        else:
+        elif not rubric.isOutsideMain():
             rubric.incrementErrorCount("BRACES_ERROR", line_num)
 
+        #if both of these are true, they are not consistent, therefore error.
         if rubric.notEgyptian:
             if rubric.egyptian:
                 rubric.incrementErrorCount("BRACES_ERROR", line_num)
 
 
-def indentationStation(filename, clean_lines, line, line_num, operator_space_tracker, rubric):
+def indentationStation(filename, clean_lines, line, line_num,
+                       operator_space_tracker, rubric):
 
     tab_size = 4
     code = clean_lines.lines[line_num]
     stripped_code = code.strip()
-    function = re.search(r'(\w(\w|::|\*|\&|\s)*)\(', code)
+    function = isFunction(code)
     if_statement = re.search(r'^if\s*\(\s*', stripped_code)
     else_if_statement = re.search(r'^else\s*\(', code)
     else_statement = re.search(r'^else\s+', code)
     switch_statement = re.search(r'^switch\s*\(', stripped_code)
 
     indentation = re.search(r'^( *)\S', code)
-    indentation = indentation.group()
-    indentation_size = len(indentation) - len(indentation.strip())
+    if indentation:
+        indentation = indentation.group()
+        indentation_size = len(indentation) - len(indentation.strip())
+    else:
+        return
 
     if function or rubric.isOutsideMain():
         if indentation_size != 0:
@@ -323,14 +328,17 @@ def indentationStation(filename, clean_lines, line, line_num, operator_space_tra
 
     #TODO: Need to check indentation ON the same line as the function still
     if function:
-        if not code.find('{'):
+        #if not egyptian style
+        if code.find('{') == -1:
             second_line = clean_lines.lines[line_num + 1]
 
             if code.find('{'):
-                temp_line_num = line_num
+                temp_line_num = line_num + 1
                 data_structure_tracker = DataStructureTracker()
+                data_structure_tracker.brace_stack.append('{')
                 checkCurrentBlockIndentation(indentation, tab_size, code, rubric,
-                                         clean_lines, data_structure_tracker, temp_line_num)
+                                         clean_lines, data_structure_tracker,
+                                         temp_line_num)
             else:
                 #TODO Figure out what it means to not have braces in the right place
                 pass
@@ -339,27 +347,8 @@ def indentationStation(filename, clean_lines, line, line_num, operator_space_tra
             data_structure_tracker = DataStructureTracker()
             data_structure_tracker.brace_stack.append('{')
             checkCurrentBlockIndentation(indentation, tab_size, code, rubric,
-                                         clean_lines, data_structure_tracker, temp_line_num)
-    elif if_statement:
-        temp_line_num = line_num
-        data_structure_tracker = DataStructureTracker()
-        checkCurrentBlockIndentation(indentation, tab_size, code, rubric,
-                                         clean_lines, data_structure_tracker, temp_line_num)
-    elif switch_statement:
-        temp_line_num = line_num
-        data_structure_tracker = DataStructureTracker()
-        checkCurrentBlockIndentation(indentation, tab_size, code, rubric,
-                                         clean_lines, data_structure_tracker, temp_line_num)
-    elif else_if_statement:
-        temp_line_num = line_num
-        data_structure_tracker = DataStructureTracker()
-        checkCurrentBlockIndentation(indentation, tab_size, code, rubric,
-                                         clean_lines, data_structure_tracker, temp_line_num)
-    elif else_statement:
-        temp_line_num = line_num
-        data_structure_tracker = DataStructureTracker()
-        checkCurrentBlockIndentation(indentation, tab_size, code, rubric,
-                                         clean_lines, data_structure_tracker, temp_line_num)
+                                         clean_lines, data_structure_tracker,
+                                         temp_line_num)
     else:
         return
 
@@ -375,19 +364,22 @@ def checkCurrentBlockIndentation(indentation, tab_size, code, rubric, clean_line
 
     while data_structure_tracker.isInBlock():
         temp_line_num += 1
-        current_indentation = re.search(r'^( *)\S', clean_lines.lines[temp_line_num])
-        line_start = current_indentation.group()
-        current_indentation = len(line_start) - len(line_start.strip())
-        if current_indentation != next_indentation and line_start.find('}') == -1:
-            rubric.incrementErrorCount("INDENTATION_ERROR", temp_line_num)
-        if clean_lines.lines[temp_line_num].find("{") != -1:
-            data_structure_tracker.addBrace("{")
-            next_indentation = current_indentation + tab_size
-        elif clean_lines.lines[temp_line_num].find("}") != -1:
-            data_structure_tracker.popBrace()
-            next_indentation = current_indentation - tab_size
+        current_indentation = re.search(r'^( *)\S',
+                                        clean_lines.lines[temp_line_num])
+        if current_indentation:
+            line_start = current_indentation.group()
+            current_indentation = len(line_start) - len(line_start.strip())
+            if current_indentation != next_indentation and line_start.find('}') == -1:
+                rubric.incrementErrorCount("INDENTATION_ERROR", temp_line_num)
+            if clean_lines.lines[temp_line_num].find("{") != -1:
+                data_structure_tracker.addBrace("{")
+                next_indentation = current_indentation + tab_size
+            elif clean_lines.lines[temp_line_num].find("}") != -1:
+                data_structure_tracker.popBrace()
+                next_indentation = next_indentation - tab_size
 
-def parseCurrentLine(filename, clean_lines, line, line_num, operator_space_tracker, rubric):
+def parseCurrentLine(filename, clean_lines, line, line_num,
+                     operator_space_tracker, rubric):
 
     #Clear
     nonConstGlobal(filename, clean_lines, line_num, rubric)
@@ -406,7 +398,8 @@ def parseCurrentLine(filename, clean_lines, line, line_num, operator_space_track
     operatorSpacing(filename, clean_lines, line, line_num, operator_space_tracker, rubric)
 
     #Call function or checking INDENTATION!!
-    indentationStation(filename, clean_lines, line, line_num, operator_space_tracker, rubric) #TODO THIS IS UNDER CONSTRUCTION
+    indentationStation(filename, clean_lines, line, line_num, operator_space_tracker,
+                       rubric) #TODO THIS IS UNDER CONSTRUCTION
     #Check for braces being consistent (egyptian or non-egyptian)
     braceConsistency(clean_lines, line, line_num, rubric)
 
@@ -428,7 +421,8 @@ def gradeStudentFile(filename, rubric, operator_space_tracker):
         newline = False
         x = 0
         for x in range(x, len(student_code)):
-            ending = student_code[x][-1]
+            if len(student_code[x]) > 0:
+                ending = student_code[x][-1]
             if ending == '\r':
                 student_code[x] = student_code[x].rstrip('\r')
                 newline = True
@@ -444,7 +438,8 @@ def gradeStudentFile(filename, rubric, operator_space_tracker):
 
     except IOError:
         sys.stderr.write(
-            "This file could not be read: '%s.'  Please check filename and resubmit \n" % filename)
+            "This file could not be read: '%s.'  "
+            "Please check filename and resubmit \n" % filename)
         return
 
     processStudentFile(student_code, rubric, filename, operator_space_tracker)
@@ -463,26 +458,12 @@ def getArguments(argv):
 
     return argv
 
-# def isIfStatement(code):
-#     returntype = (Literal("void") | Literal('int') | Literal('string') |
-#                   Literal('double') | Literal('float') | Literal('char'))
-#     function_name = Word(alphanums + '_')
-#     args= Word(alphanums + ',' + ' ')
-#     function_open = Literal("{")
-#     function_close = Literal("}")
-#     function_decl = returntype + function_name + "(" + Optional(args) + ")"
-#     grammar = function_decl + Optional(function_open)
-#
-#     s = "void function_Name12(int one, int two)"
-#
-#     try:
-#         data = grammar.parseString(s)
-#     except ParseBaseException:
-#         d = ParseBaseException
+
 
 def isFunction(code):
 
-    returntype = (Literal("void") | Literal('int') | Literal('string') |Literal('double') | Literal('float') | Literal('char'))
+    returntype = (Literal("void") | Literal('int') | Literal('string')
+                  |Literal('double') | Literal('float') | Literal('char'))
     function_name = Word(alphanums + '_')
     args= Word(alphanums + ',' + ' ')
     function_open = Literal("{")
@@ -498,7 +479,6 @@ def isFunction(code):
 
 def main():
 
-    print "Hello World"
     operator_space_tracker = OperatorSpace()
     rubric = StyleRubric()
     student_file_names = getArguments(sys.argv[1:])
