24db8e9f911d9f99f68b9f4e6747dda335af4ece
Steve Merritt <smerritt@barracuda.com>
2014-06-29 14:06:31 -0400

    Finish basic OO of StyleRubric, StyleError

---
+++ StyleError.py
@@ -0,0 +1,59 @@
+class StyleError(object):
+    """
+    Represents a style error in the student's code.
+    """
+
+    def __init__(self):
+        self.line_num = 0
+        self.label = ""
+        self.points_worth = 0
+
+    def __init__(self, points, label, line_num):
+        """
+        Log the line number, type and point value of a specific error.
+        points (int): Weight of this error.
+        label (str): Key for response lookup in list_of_errors.
+        line_num (int): Line number of this error.
+        """
+
+        list_of_errors = {
+            "OPERATOR_SPACE_ERROR": "Incorrect spacing around operators",
+            "INDENTATION_ERROR": "Incorrect indentation. Check to make sure you are four spaces in from previous code block.",
+            "COMMAND_ERROR": "There should only be one command (statement) on each line.",
+            "IF_ELSE_ERROR": "Every If-Else statement should have brackets.",
+            "GLOBAL_VARIABLE": "You should never have a non-const global variable.",
+            "FUNCTION_LENGTH_ERROR": "Your function is too long. Break it up into separate functions.",
+            "LINE_WIDTH": "Line limit of 80 characters exceeded.",
+            "BOOL_VALUE": "You need to return true or false, instead of an actual number.",
+            "MAGIC_NUMBER": "Store numbers in variables, so that you can give them meaningful names.",
+            "BRACES_ERROR": "Your braces should be either Egyptian or block style, pick one.",
+            "SPACING_ERROR": "Use tabs or spaces, not both.",
+            "UNNECESSARY_BREAK": "Breaks should ONLY be used in switch statements. Fix your logic.",
+            "GOTO": "Never use the goto function.",
+            "DEFINE_STATEMENT": "While define statements have their applications, we do not allow them in EECS 183.",
+            "EQUALS_TRUE": "It is stylistically preferred to use 'if (x)' instead of 'if (x == true)'.",
+            "WHILE_TRUE": "It is almost always preferred to use an explicit conditional instead of 'while(true)'.",
+            "TERNARY_OPERATOR": "The use of ternary expressions (e.g. return f(x) ? true : false) is discouraged in EECS 183.",
+            "CONTINUE_STATEMENT": "While 'continue' is occasionally appropriate, we discourage its use in EECS 183.",
+            "MAIN_SYNTAX": "Your declaration of main() does not adhere to conventional stylistic guidelines.",
+        }
+
+        self.set_points_worth(points)
+        self.set_line_num(line_num)
+        self.set_label(list_of_errors[label])
+        print list_of_errors[label]
+
+    def set_line_num(self, line):
+        self.line_num = line
+    def set_label(self, label_name):
+        self.label = label_name
+    def set_points_worth(self, points):
+        self.points_worth = points
+    def get_points(self):
+        return self.points_worth
+    def get_label(self):
+        return self.label
+    def get_line_number(self):
+        return self.line_num
+
+
--- style_grader_functions.py
+++ style_grader_functions.py
@@ -1,374 +1,6 @@
-
-
-def valid_return(clean_lines, line_num, rubric):
-    code = clean_lines.lines
-    current_line = code[line_num]
-    returnVal = re.search(r'\s+return\s+', current_line)
-
-    if returnVal: 
-        # make sure it's not the end of a word
-        list_of_line = current_line.split(' ')
-        if len(list_of_line) > 2:
-            #TODO Figure out what you want to take off with a more than one statement on return line
-            pass
-        else:
-            current_line = current_line[6:].strip()
-            if current_line.isdigit():
-                rubric.add_error("BOOL_VALUE", line_num)
-
-
-def num_of_commands(clean_lines, line_num, rubric):
-    cleansed_line = clean_lines.lines[line_num]
-    # This code is taken directly from cpplint lines 3430-3440
-    if (cleansed_line.count(';') > 1 and
-        # for loops are allowed two ;'s (and may run over two lines).
-        cleansed_line.find('for') == -1 and
-        (GetPreviousNonBlankLine(clean_lines, line_num)[0].find('for') == -1 or
-        GetPreviousNonBlankLine(clean_lines, line_num)[0].find(';') != -1) and
-        # It's ok to have many commands in a switch case that fits in 1 line
-        not ((cleansed_line.find('case ') != -1 or
-        cleansed_line.find('default:') != -1) and
-        cleansed_line.find('break;') != -1)):
-        
-        rubric.add_error("COMMAND_ERROR", line_num)
-
-
-def line_width_check(line, rubric):
-    max_length = 80
-    current_length = len(line)
-    if current_length > max_length:
-        rubric.add_error("LINE_WIDTH", line_num)
-
-
-def operator_spacing(clean_lines, line_num, operator_space_tracker, rubric):
-    code = clean_lines.lines[line_num]
-    matches = []
-    if not check_operator_regex(code, '\+')  or \
-        not check_operator_regex(code, '\-') or \
-        not check_operator_regex(code, '\/') or \
-        not check_operator_regex(code, '\%') or \
-        not check_operator_regex(code, '\*'):
-        rubric.add_error("OPERATOR_SPACE_ERROR", line_num)
-
-    else:
-        return True
-
-def check_equals_true(clean_lines, line_num, rubric):
-    code = clean_lines.lines[line_num]
-    variable = Word(alphanums)
-    keyword = Literal("true") | Literal("false")
-    statement_parser = Group(variable + "==" + keyword) | Group(keyword + "==" + variable)
-    if len(statement_parser.searchString(code)):
-        rubric.add_error("EQUALS_TRUE", line_num) 
-
-def check_operator_regex(code, operator):
-    regex_one = r'' + '\S+' + operator
-    regex_two =  r'' + operator + '\S+'
-
-
-
-    #check to see if there is a non-whitespace character on either side of the operator
-    if re.search(regex_one, code) or re.search(regex_two, code):
-
-        left_not_wspace = re.search(regex_one, code)
-        right_not_wspace = re.search(regex_two, code)
-
-        if right_not_wspace and left_not_wspace:
-            left_code = left_not_wspace.group()
-            right_code = right_not_wspace.group()
-            left_symbol = left_code[-2]
-            right_symbol = right_code[1]
-            operator = right_symbol[0]
-
-            if operator == '+' or operator == '-':
-                if right_symbol == '-' or right_symbol == '=' or right_symbol == '+':
-                    return  True
-                if left_symbol == '-' or left_symbol == '=' or left_symbol == '+':
-                    return True
-            elif operator == '/':
-                if right_symbol == '=':
-                    return True
-            elif operator == '=':
-                if left_symbol == '+' or left_symbol == '-':
-                    return True
-                if right_symbol or left_symbol == '=':
-                    return True
-
-            else:
-                return False
-    else:
-        return True
-
-
-def check_go_to(clean_lines, line_num, rubric):
-    code = clean_lines.lines[line_num]
-
-    match = re.search(r'(\s+|^)goto\s+', code)
-    if match :
-        rubric.add_error("GOTO", line_num)
-
-def check_define_statements(clean_lines, line_num, rubric):
-    code = clean_lines.lines[line_num]
-    
-    match = re.search(r'(\s+|^)#(\s*)define(\s+)', code)
-    if match:
-        rubric.add_error("DEFINE_STATEMENT", line_num)
-
-def check_continue_statements(clean_lines, line_num, rubric):
-    code = clean_lines.lines[line_num]
-    if len(Literal("continue").searchString(code)):
-        rubric.add_error("CONTINUE_STATEMENT", line_num)
-
-def check_ternary_operator(clean_lines, line_num, rubric):
-    # This is really easy - ternary operators require the conditional operator "?",
-    # which has no other application in C++. Since we're parsing out comments, it's as easy as:
-    code = clean_lines.lines[line_num]
-    if len(Literal("?").searchString(code)):
-        rubric.add_error("TERNARY_OPERATOR", line_num)
-
-def check_while_true(clean_lines, line_num, rubric):
-    code = clean_lines.lines[line_num]
-    statement_parser = Literal("while") + Literal("(") + Literal("true") + Literal(")")
-    if len(statement_parser.searchString(code)):
-        rubric.add_error("WHILE_TRUE", line_num)
-
-def check_non_const_global(clean_lines, line_num, rubric):
-    code = clean_lines.lines[line_num]
-
-    if re.search(r'int main', code):
-        rubric.set_inside_main()
-
-    if rubric.is_outside_main():
-        function = check_if_function(code)
-        variable = re.search(r'^\s*(int|string|char|bool)\s+', code)
-        if not function and variable:
-            rubric.add_error("GLOBAL_VARIABLE", line_num)
-
-
-def check_brace_consistency(clean_lines, line_num, rubric):
-    code = clean_lines.lines[line_num]
-    stripped_code = code.strip()
-    function = check_if_function(code)
-    if_statement = re.search(r'^if\s*\(\s*', stripped_code)
-    else_if_statement = re.search(r'^else\s*\(', code)
-    else_statement = re.search(r'^else\s+', code)
-    switch_statement = re.search(r'^switch\s*\(', stripped_code)
-
-    if function or if_statement or else_statement or switch_statement:
-
-        if function and clean_lines.lines[line_num + 1].find('{') != -1 or\
-            else_if_statement and clean_lines.lines[line_num + 1].find('{') != -1 or\
-            else_statement and clean_lines.lines[line_num + 1].find('{') != -1 or\
-            switch_statement and clean_lines.lines[line_num + 1].find('{') != -1 or\
-                if_statement and clean_lines.lines[line_num + 1].find('{') != -1:
-
-            rubric.set_egyptian_style(False)
-        elif function and code.find('{') != -1 or \
-                else_if_statement and code.find('{') != -1 or\
-                else_statement and code.find('{') != -1 or\
-                switch_statement and code.find('{') != -1 or\
-                if_statement and code.find('{') != -1:
-
-            rubric.set_egyptian_style(True)
-        elif not rubric.is_outside_main():
-            rubric.add_error("BRACES_ERROR", line_num)
-
-        #if both of these are true, they are not consistent, therefore error.
-        if rubric.not_egyptian:
-            if rubric.egyptian:
-                rubric.add_error("BRACES_ERROR", line_num)
-
-
-def check_function_block_indentation(filename, clean_lines, line_num,
-                                     operator_space_tracker, rubric):
-
-    tab_size = 4
-    code = clean_lines.lines[line_num]
-    stripped_code = code.strip()
-    function = check_if_function(code)
-    if_statement = re.search(r'^if\s*\(\s*', stripped_code)
-    else_if_statement = re.search(r'^else\s*\(', code)
-    else_statement = re.search(r'^else\s+', code)
-    switch_statement = re.search(r'^switch\s*\(', stripped_code)
-
-    indentation = re.search(r'^( *)\S', code)
-    if indentation:
-        indentation = indentation.group()
-        indentation_size = len(indentation) - len(indentation.strip())
-    else:
-        return
-
-    if function or rubric.is_outside_main():
-        if indentation_size != 0:
-            rubric.add_error("INDENTATION_ERROR", line_num)
-        if rubric.is_outside_main():
-            return
-
-    #TODO: Need to check indentation ON the same line as the function still
-    if function:
-        #if not egyptian style
-        if code.find('{') == -1:
-            second_line = clean_lines.lines[line_num + 1]
-
-            if code.find('{'):
-                temp_line_num = line_num + 1
-                data_structure_tracker = DataStructureTracker()
-                data_structure_tracker.brace_stack.append('{')
-                process_current_blocks_indentation(indentation, tab_size, code, rubric,
-                                         clean_lines, data_structure_tracker,
-                                         temp_line_num)
-            else:
-                #TODO Figure out what it means to not have braces in the right place
-                pass
-        else:
-            temp_line_num = line_num
-            data_structure_tracker = DataStructureTracker()
-            data_structure_tracker.brace_stack.append('{')
-            process_current_blocks_indentation(indentation, tab_size, code, rubric,
-                                               clean_lines, data_structure_tracker,
-                                               temp_line_num)
-    else:
-        return
-
-def check_main_prefix(clean_lines, line_num, rubric):
-    #Return value for main is optional in C++11
-    parser = Literal("main")+Literal("(")+SkipTo(Literal(")"))+Literal(")")+Literal("{")
-    if len(parser.searchString(clean_lines.lines[line_num])):
-        code = clean_lines.lines[line_num]
-        main_prefix = Literal("main")+Literal("(")
-        full_use = "int"+Word(alphanums)+","+"char*"+Word(alphanums)+"["+"]"+")"
-        # 3 options for main() syntax
-        if not len((main_prefix+Literal(")")).searchString(code)) and \
-           not len((main_prefix+Literal("void")+Literal(")")).searchString(code)) and \
-           not len((main_prefix+full_use).searchString(code)):
-            rubric.add_error("MAIN_SYNTAX", line_num) 
-
-def process_current_blocks_indentation(indentation, tab_size, code, rubric, clean_lines,
-                                       data_structure_tracker, temp_line_num):
-
-    indentation = re.search(r'^( *)\S', clean_lines.lines[temp_line_num])
-    indentation = indentation.group()
-    indentation_size = len(indentation) - len(indentation.strip())
-    data_structure_tracker.set_is_in_block(True)
-    next_indentation = indentation_size + tab_size
-
-    while data_structure_tracker.check_is_in_block():
-        temp_line_num += 1
-        current_indentation = re.search(r'^( *)\S',
-                                        clean_lines.lines[temp_line_num])
-        if current_indentation:
-            line_start = current_indentation.group()
-            current_indentation = len(line_start) - len(line_start.strip())
-            if current_indentation != next_indentation and line_start.find('}') == -1:
-                rubric.add_error("INDENTATION_ERROR", temp_line_num)
-            if clean_lines.lines[temp_line_num].find("{") != -1:
-                data_structure_tracker.add_brace("{")
-                next_indentation = current_indentation + tab_size
-            elif clean_lines.lines[temp_line_num].find("}") != -1:
-                data_structure_tracker.pop_brace()
-                next_indentation = next_indentation - tab_size
-
-
-def parse_current_line_of_code(filename, clean_lines, line, line_num,
-                               operator_space_tracker, rubric):
-   
-    #Check for proper main() declaration (if main is present on this line)
-    check_main_prefix(clean_lines, line_num, rubric)
-    #Check non const globals
-    check_non_const_global(clean_lines, line_num, rubric)
-     #Only one statement on the return line?
-    valid_return(clean_lines, line_num, rubric)
-    #One statement per line
-    num_of_commands(clean_lines, line_num, rubric)
-    #check to see if the line contains a goto function
-    check_go_to(clean_lines, line_num, rubric)
-    #Check for mixed tabs/spaces and log error #TODO: This can wait
-    line_width_check(line, rubric)
-    #Check for #define statements
-    check_define_statements(clean_lines, line_num, rubric)
-    #Check for "== true" statements
-    check_equals_true(clean_lines, line_num, rubric)
-    #Check for "while(true)" statements
-    check_while_true(clean_lines, line_num, rubric)
-    #Check ternary expressions
-    check_ternary_operator(clean_lines, line_num, rubric)
-    #Check continue statements
-    check_continue_statements(clean_lines, line_num, rubric)
-
-    
-    #Check for unnecessary includes
-    #TODO: Above duh.
-
-    #Check for operator Spacing
-    operator_spacing(clean_lines, line_num, operator_space_tracker, rubric)
-
-    #Call function or checking INDENTATION!!
-    check_function_block_indentation(filename, clean_lines, line_num, operator_space_tracker,
-                       rubric)
-                       #TODO THIS IS UNDER CONSTRUCTION
-    #Check for braces being consistent (egyptian or non-egyptian)
-    check_brace_consistency(clean_lines, line_num, rubric)
-
-
-def process_current_student_file(student_code, rubric, filename, operator_space_tracker):
-    error = ""
-    RemoveMultiLineComments(filename, student_code, error)
-    clean_lines = CleansedLines(student_code)
-    code = clean_lines.lines
-    line_num = 0
-    for line in code:
-        parse_current_line_of_code(filename, clean_lines, line, line_num, operator_space_tracker, rubric)
-        line_num += 1
-
-
-def grade_student_file(filename, rubric, operator_space_tracker):
-    print "Grading student submission: {}".format(filename)
-    try:
-        student_code = codecs.open(filename, 'r', 'utf8', 'replace').read().split('\n')
-        newline = False
-        x = 0
-        ending = ''
-        for x in range(x, len(student_code)):
-            if len(student_code[x]) > 0:
-                ending = student_code[x][-1]
-            if ending == '\r':
-                student_code[x] = student_code[x].rstrip('\r')
-                newline = True
-            else:
-                newline = False
-
-        # This avoids getting the period character
-
-        location = filename.find('.') + 1
-        extension = filename[location:]
-
-        if extension not in ['h', 'cpp']:
-            sys.stderr.write("Incorrect file type\n")
-            return
-
-    except IOError:
-        sys.stderr.write(
-            "This file could not be read: '%s.'  "
-            "Please check filename and resubmit \n" % filename)
-        return
-
-    process_current_student_file(student_code, rubric, filename, operator_space_tracker)
-    return
-
-
-def get_arguments(argv):
-    try:
-        (opts, filenames) = getopt.getopt(argv, '', ['whitespace=', 'globalconst=', 'verbose=',
-                                                     'counting=',
-                                                     'filter=',
-                                                     'root=',
-                                                     'linelength=',
-                                                     'extensions='])
-    except getopt.GetoptError:
-        print('Invalid arguments.')
-
-    return argv
-
+from pyparsing import Literal, Word, Optional, ParseException, Group, SkipTo, alphanums
+import getopt
+import re
 
 def check_if_function(code):
 
@@ -386,3 +18,47 @@ def check_if_function(code):
         return True
     except ParseException:
         return False
+
+def get_arguments(argv):
+    try:
+        (opts, filenames) = getopt.getopt(argv, '', ['whitespace=', 'globalconst=', 'verbose=',
+                                                     'counting=',
+                                                     'filter=',
+                                                     'root=',
+                                                     'linelength=',
+                                                     'extensions='])
+    except getopt.GetoptError:
+        print('Invalid arguments.')
+
+    return argv
+
+def check_operator_regex(code, operator):
+    regex_one = r'' + '\S+' + operator
+    regex_two =  r'' + operator + '\S+'
+    #check to see if there is a non-whitespace character on either side of the operator
+    if re.search(regex_one, code) or re.search(regex_two, code):
+        left_not_wspace = re.search(regex_one, code)
+        right_not_wspace = re.search(regex_two, code)
+        if right_not_wspace and left_not_wspace:
+            left_code = left_not_wspace.group()
+            right_code = right_not_wspace.group()
+            left_symbol = left_code[-2]
+            right_symbol = right_code[1]
+            operator = right_symbol[0]
+            if operator == '+' or operator == '-':
+                if right_symbol == '-' or right_symbol == '=' or right_symbol == '+':
+                    return  True
+                if left_symbol == '-' or left_symbol == '=' or left_symbol == '+':
+                    return True
+            elif operator == '/':
+                if right_symbol == '=':
+                    return True
+            elif operator == '=':
+                if left_symbol == '+' or left_symbol == '-':
+                    return True
+                if right_symbol or left_symbol == '=':
+                    return True
+            else:
+                return False
+    else:
+        return True
