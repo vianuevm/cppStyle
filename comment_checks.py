097d6f4989ad5f6deeedec7d027e91bbeb5aeede
Steve Merritt <smerritt@barracuda.com>
2014-07-09 01:24:35 -0400

    Merge Maxim's commits from (#64)

--- comment_checks.py
+++ comment_checks.py
@@ -1,4 +1,5 @@
 import re
+from pyparsing import Word, Literal, alphanums
 
 def check_line_width(self, line):
     max_length = 80
@@ -7,14 +8,18 @@ def check_line_width(self, line):
         self.add_error(label="LINE_WIDTH", data={'length': current_length})
 
 def check_missing_rme(self, lines):
-    requires = effects = modifies = False
-    #Check if there's a complete RME in the last 10 lines
-    for line_num in range(self.current_line_num - 10, self.current_line_num):
-        code = lines[line_num].lower()
-        if re.search('requires', code): requires = True
-        if re.search('effects', code): effects = True
-        if re.search('modifies', code): modifies = True
-    # If it's not there, maybe they defined it in a header file.
-    # Finish this later
-    if not (requires and effects and modifies):
-        self.add_error(label="MISSING_RME")
+    function = Word(alphanums + '_')
+    function_syntax = function + Literal('(')
+    parsed = function_syntax.searchString(lines[self.current_line_num]).asList()
+    function_name = parsed[0][0]
+    if function_name != 'main':
+        requires = effects = modifies = False
+        #Check if there's a complete RME in the last 10 lines
+        for line_num in range(self.current_line_num - 10, self.current_line_num):
+            code = lines[line_num].lower()
+            if re.search('requires', code): requires = True
+            if re.search('effects', code): effects = True
+            if re.search('modifies', code): modifies = True
+        # If it's not there, maybe they defined it in a header file. #TODO
+        if not (requires and effects and modifies):
+            self.add_error("MISSING_RME", data={'function': function_name})
