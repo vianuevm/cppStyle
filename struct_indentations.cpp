8871b8969c1e2a673a04753d72fd8d6842e2426b
mv <michael.vianueva@thomsonreuters.com>
2014-07-14 09:36:00 -0400

    add struct indentation cpp

---
+++ struct_indentations.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+class Student{
+    public:
+        int id;
+    private:
+        string dorm(string room, string isSmall);
+}
+
+struct MyStruct{
+    int x;
+    private:
+        int arg;
+        bool why;
+    public:
+        string poop;
+        int x(int y, int z);
+};
+
+int main(){
+    switch(arg){
+        case "hello":
+            break;
+        case "goodbye":
+            break;
+    }
+    return 0;
+}
+
+
+
