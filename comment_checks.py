9e3e5cee97549240d302905994db0e58d2ed4720
maxim123 <maximal@umich.edu>
2014-07-21 21:42:35 +0200

    Removed a TODO

--- comment_checks.py
+++ comment_checks.py
@@ -22,7 +22,6 @@ def check_missing_rme(self, lines):
             if re.search('effects', code): effects = True
             if re.search('modifies', code): modifies = True
         # If it's not there, maybe they defined it in a header file.
-        # TODO: check only #included files.
         if not (requires and effects and modifies) and (function_signature not in self.all_rme[self.current_file]):
             # error only in this case
             # prevent double-counting
