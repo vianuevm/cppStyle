30a38f9e02319056cacb59b45e9515f8336c65e2
Steve Merritt <smerritt@barracuda.com>
2014-08-07 19:09:48 -0400

    Clean up a bunch of garbage

--- regression_test.py
+++ regression_test.py
@@ -18,20 +18,15 @@ Name each test after the function being tested
 Decorator to load the appropriate lines so there's no error collision.
 Use it as @load_code_segment(start_line, end_line) to load code lines in range [start_line, end_line)
 '''
-def load_code_segment(start, end):
+def load_code_segment(filename):
     def wrapper(func):
         def fn(self, *args, **kwargs):
             #Redirect stdout because it's annoying
-            tempout,temperr = sys.stdout, sys.stderr
-            sys.stdout = sys.stderr = open(os.devnull, 'w')
-            testfile = open('testing_file.cpp', 'w+')
-            source = open('hello_reg_test.cpp', 'rU').readlines()
-            for i in range(start-1, end-1):
-                testfile.write(source[i])
-            testfile.close()
+            #tempout,temperr = sys.stdout, sys.stderr
+            #sys.stdout = sys.stderr = open(os.devnull, 'w')
             self.rubric = StyleRubric()
-            self.rubric.grade_student_file('testing_file.cpp')
-            sys.stdout, sys.stderr = tempout, temperr  
+            self.rubric.grade_student_file('test_source/'+filename)
+            #sys.stdout, sys.stderr = tempout, temperr  
             return func(self, *args, **kwargs)
         return fn
     return wrapper
@@ -49,17 +44,18 @@ class RegressionTesting(unittest.TestCase):
             print x,y
         print "-----------------------------\n\n"
 
-    @load_code_segment(21,56)
-    def test_good_file(self): self.assertTrue(not len(self.rubric.error_tracker[self.rubric.current_file]))
-    @load_code_segment(1,10)
+
+    @load_code_segment('good.cpp')
+    def test_good_file(self): self.assertTrue(not len(self.rubric.error_tracker))
+    @load_code_segment('num_of_commands.cpp')
     def test_statements_per_line(self): self.assertEqual(self.rubric.error_types['STATEMENTS_PER_LINE'], 3)
-    @load_code_segment(11,20)
+    @load_code_segment('test_valid_return.cpp')
     def test_int_for_bool(self): self.assertEqual(self.rubric.error_types['INT_FOR_BOOL'], 2)
-    @load_code_segment(57,79)
+    @load_code_segment('if_else_good.cpp')
     def test_good_if_else(self): self.assertRaises(KeyError, self.rubric.error_types.get, 'IF_ELSE_ERROR')
-    @load_code_segment(80,89)
-    def test_bad_if_else(self): self.assertEqual(3, self.rubric.error_types['IF_ELSE_ERROR'])
-    @load_code_segment(90,95)
+    #@load_code_segment('if_else_bad.cpp')
+    #def test_bad_if_else(self): self.assertEqual(3, self.rubric.error_types['IF_ELSE_ERROR'])
+    @load_code_segment('equals_true.cpp')
     def test_equals_true(self): self.assertEqual(5, self.rubric.error_types['EQUALS_TRUE']) 
 
 if __name__ == '__main__':
