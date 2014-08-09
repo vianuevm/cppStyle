7d7ff335ce545470f8ae26855ae8d0f41e7f5054
Mike Vianueva <vianuevm@umich.edu>
2014-08-09 16:11:54 -0400

    brunt of backend for website

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from app import views, models
