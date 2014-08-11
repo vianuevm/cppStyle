89d6c2cf74e21b142b67c031e083d92cbf79bcdb
Mike Vianueva <vianuevm@umich.edu>
2014-08-10 22:23:38 -0400

    Changed style grader to reflect a web application, now called from the POST

--- style_grader_main.py
+++ style_grader_main.py
@@ -4,6 +4,7 @@ from StyleRubric import StyleRubric
 
 def grader(online_files):
     rubric = StyleRubric()
+    show_errors = []
 
     # if online_submit:
     for filename in online_files:
@@ -13,16 +14,11 @@ def grader(online_files):
     #         rubric.grade_student_file(filename)
 
     rubric.adjust_errors()
-    rubric.print_errors()
+    show_errors = rubric.print_errors(show_errors)
 #For debugging purposes only
-    show_errors = ""
     print "Total Errors: " + str(rubric.total_errors)
     for x, y in rubric.error_types.items():
         print x, y
-        show_errors += x
-        show_errors += " "
-        show_errors += str(y)
-        show_errors += '\n'
 
 
     return show_errors
