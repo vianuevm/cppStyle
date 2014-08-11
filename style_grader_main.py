4383b2a8adf873d958670042970a6c73f620407b
Mike Vianueva <vianuevm@umich.edu>
2014-08-10 20:40:52 -0400

    Fooling around with getting files passed to main program

--- style_grader_main.py
+++ style_grader_main.py
@@ -2,20 +2,36 @@
 from style_grader_functions import print_success
 from StyleRubric import StyleRubric
 
-def main():
+def grader(online_submit, online_files):
+
+
     rubric = StyleRubric()
 
-    for filename in rubric.student_files:
+
+    if online_submit:
+       for filename in online_files:
         rubric.grade_student_file(filename)
+    else:
+        for filename in rubric.student_files:
+            rubric.grade_student_file(filename)
 
     rubric.adjust_errors()
 
     rubric.print_errors()
 
+
 #For debugging purposes only
+    show_errors = ""
     print "Total Errors: " + str(rubric.total_errors)
     for x, y in rubric.error_types.items():
         print x, y
+        show_errors += x
+        show_errors += " "
+        show_errors += str(y)
+        show_errors += '\n'
+
+
+    return show_errors
  
 if __name__ == '__main__':
     main()
