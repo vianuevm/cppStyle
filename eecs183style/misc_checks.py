fe60909fa3e23ab8372c2201297eda07e90e749a
Mike Vianueva <vianuevm@umich.edu>
2014-08-13 18:57:55 -0400

    merging

--- eecs183style/misc_checks.py
+++
@@ -1,8 +0,0 @@
-def check_pointer_reference_consistency(self):
-    if self.spacer.asts_left:
-        if self.spacer.amps_right or self.spacer.amps_both:
-            self.add_error(label='POINTER_REFERENCE_CONSISTENCY')
-    elif self.spacer.asts_right:
-        if self.spacer.amps_left or self.spacer.amps_both:
-            self.add_error(label='POINTER_REFERENCE_CONSISTENCY')
-
