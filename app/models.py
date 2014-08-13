13b3b4d710b492f6a2708f0efa0677afa9c6404b
Mike Vianueva <vianuevm@umich.edu>
2014-08-13 00:05:10 -0400

    removal of a lot

--- app/models.py
+++ app/models.py
@@ -1,38 +1,38 @@
-from app import db
+# from app import db
 
-FALSE = 0
-TRUE = 1
+# FALSE = 0
+# TRUE = 1
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(65))
-    passed_grader = db.Column(db.SmallInteger, default = FALSE)
-    email = db.Column(db.String(120), unique = True)
-    umich_id = db.Column(db.Integer, unique = True)
-    posts = db.relationship('Submission')
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String(65))
+#     passed_grader = db.Column(db.SmallInteger, default = FALSE)
+#     email = db.Column(db.String(120), unique = True)
+#     umich_id = db.Column(db.Integer, unique = True)
+#     posts = db.relationship('Submission')
 
-    def is_authenticated(self):
-        return True
+#     def is_authenticated(self):
+#         return True
 
-    def is_active(self):
-        return True
+#     def is_active(self):
+#         return True
 
-    def is_anonymous(self):
-        return False
+#     def is_anonymous(self):
+#         return False
 
-    def get_id(self):
-        return unicode(self.id)
+#     def get_id(self):
+#         return unicode(self.id)
 
-    def __repr__(self):
-        return '<User %r>' % (self.name)
+#     def __repr__(self):
+#         return '<User %r>' % (self.name)
 
-class Submission(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(65))
-    umich_id = db.Column(db.Integer, unique = True)
-    passed_grader = db.Column(db.SmallInteger, default = False)
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+# class Submission(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String(65))
+#     umich_id = db.Column(db.Integer, unique = True)
+#     passed_grader = db.Column(db.SmallInteger, default = False)
+#     timestamp = db.Column(db.DateTime)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __repr__(self):
-        return '<Post %r>' % (self.umich_id)
+#     def __repr__(self):
+#         return '<Post %r>' % (self.umich_id)
--- app/views.py
+++ app/views.py
@@ -1,18 +1,15 @@
 import os
-from flask import render_template, flash, redirect, session, url_for, request, g, send_from_directory
-from flask_login import login_user, logout_user, current_user, login_required
-from app import app, db, lm, oid
-from forms import LoginForm
-from models import User, Submission
+from flask import render_template, redirect, session, url_for, request, send_from_directory
+from app import app
 from werkzeug import secure_filename
 from style_grader_main import style_grader_driver
 
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['ALLOWED_EXTENSIONS'] = set(['cpp', 'h'])
 
-@app.before_request
-def before_request():
-    g.user = current_user
+# @app.before_request
+# def before_request():
+#     g.user = current_user
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -20,9 +17,8 @@ def allowed_file(filename):
 
 @app.route('/',  methods = ['GET'])
 @app.route('/index', methods = ['GET', 'POST'])
-@login_required
 def index():
-    user = g.user
+    # user = g.user
 
     if request.method == 'POST':
         # Get the FileStorage instance from request
@@ -33,8 +29,7 @@ def index():
             filename = filename,
             type = file.content_type)
     return render_template('index.html',
-        title = 'Home',
-        user = user)
+        title = 'Home')
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -53,13 +48,13 @@ def upload():
         list_of_online_files.append(online_file)
 
     response = style_grader_driver(list_of_online_files)
-    if response != []:
-        sub = Submission(user_id = g.user.id, passed_grader = False)
-    else:
-        sub =  Submission(umich_id = g.user.umich_id, user_id = g.user.id, passed_grader = True)
+    # if response != []:
+    #     sub = Submission(user_id = g.user.id, passed_grader = False)
+    # else:
+    #     sub =  Submission(umich_id = g.user.umich_id, user_id = g.user.id, passed_grader = True)
 
-    db.session.add(sub)
-    db.session.commit()
+    # db.session.add(sub)
+    # db.session.commit()
 
     return render_template('upload.html', filenames=filenames, errors=response )
 
@@ -68,46 +63,46 @@ def uploaded_file(filename):
  return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
-@app.route('/login', methods = ['GET', 'POST'])
-@oid.loginhandler
-def login():
-    if g.user is not None and g.user.is_authenticated():
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
-    return render_template('login.html',
-        title = 'Sign In',
-        form = form,
-        providers = app.config['OPENID_PROVIDERS'])
+# @app.route('/login', methods = ['GET', 'POST'])
+# @oid.loginhandler
+# def login():
+#     if g.user is not None and g.user.is_authenticated():
+#         return redirect(url_for('index'))
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         session['remember_me'] = form.remember_me.data
+#         return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
+#     return render_template('login.html',
+#         title = 'Sign In',
+#         form = form,
+#         providers = app.config['OPENID_PROVIDERS'])
 
 
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+# @lm.user_loader
+# def load_user(id):
+#     return User.query.get(int(id))
 
-@oid.after_login
-def after_login(resp):
-    if resp.email is None or resp.email == "":
-        flash('Invalid login. Please try again.')
-        return redirect(url_for('login'))
-    user = User.query.filter_by(email = resp.email).first()
-    if user is None:
-        nickname = resp.nickname
-        if nickname is None or nickname == "":
-            nickname = resp.email.split('@')[0]
-        user = User(email = resp.email, passed_grader = 0)
-        db.session.add(user)
-        db.session.commit()
-    remember_me = False
-    if 'remember_me' in session:
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(user, remember = remember_me)
-    return redirect(request.args.get('next') or url_for('index'))
+# @oid.after_login
+# def after_login(resp):
+#     if resp.email is None or resp.email == "":
+#         flash('Invalid login. Please try again.')
+#         return redirect(url_for('login'))
+#     user = User.query.filter_by(email = resp.email).first()
+#     if user is None:
+#         nickname = resp.nickname
+#         if nickname is None or nickname == "":
+#             nickname = resp.email.split('@')[0]
+#         user = User(email = resp.email, passed_grader = 0)
+#         db.session.add(user)
+#         db.session.commit()
+#     remember_me = False
+#     if 'remember_me' in session:
+#         remember_me = session['remember_me']
+#         session.pop('remember_me', None)
+#     login_user(user, remember = remember_me)
+#     return redirect(request.args.get('next') or url_for('index'))
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
+# @app.route('/logout')
+# def logout():
+#     logout_user()
+#     return redirect(url_for('index'))
