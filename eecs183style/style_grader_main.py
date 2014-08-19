05b58af9aaa8a5d00046c32e594d2099137bfcde
Waleed Khan <arxanas@gmail.com>
2014-08-19 17:42:46 -0400

    Added command-line executable and setup script.

--- eecs183style/style_grader_main.py
+++ eecs183style/style_grader_main.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
-from style_grader_functions import print_success
-from StyleRubric import StyleRubric
+from eecs183style.style_grader_functions import print_success
+from eecs183style.StyleRubric import StyleRubric
+import sys
 
 def main():
-    rubric = StyleRubric()
+    # If there are filenames passed on the command line, use those; otherwise,
+    # let the config reader get the file names.
+    if len(sys.argv) > 1:
+        student_files= sys.argv[1:]
+    else:
+        student_files = None
+
+    rubric = StyleRubric(student_files=student_files)
 
     for filename in rubric.student_files:
         rubric.grade_student_file(filename)
@@ -16,6 +24,11 @@ def main():
     print "Total Errors: " + str(rubric.total_errors)
     for x, y in rubric.error_types.items():
         print x, y
- 
+
+    if rubric.total_errors:
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
 if __name__ == '__main__':
     main()
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name="183style",
+    description="University of Michigan EECS 183 style grader.",
+    packages=["eecs183style"],
+    scripts=["bin/183style"]
+)
