149ef3e6739c8e8e76997e2492b08be77cb9b857
Waleed Khan <arxanas@gmail.com>
2014-08-20 15:10:27 -0400

    Added requirements to setup.py.

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="183style",
     description="University of Michigan EECS 183 style grader.",
+    install_requires=open("requirements.txt", "r").readlines(),
     packages=["eecs183style"],
     package_data={"eecs183style": ["rubric.ini"]},
     scripts=["bin/183style"]
