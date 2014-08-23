0099f73fff695e570fe625ab8c1cb6bb95849774
Mike Vianueva <vianuevm@umich.edu>
2014-08-23 16:06:46 -0400

    Fixed a few bugs

--- app/static/behavior/scripts.js
+++ app/static/behavior/scripts.js
@@ -16,12 +16,9 @@ YAHOO.util.Event.onDOMReady(function() {
 
 $(document).ready(function() {
 	function updateForm() {
-		if (!$('#code-input').val())
-		{
+		if (!$('#code-input').val()) {
 			$('#code-submission').attr("disabled", "disabled");
-		}
-		else
-		{
+		} else {
 			$('.code-submission').removeAttr("disabled");
 		}
 	}
@@ -30,9 +27,19 @@ $(document).ready(function() {
 		var ul = document.getElementById("errorlist");
 		var items = ul.getElementsByTagName("li");
 		var itemLength = items.length;
+		clearFileInput();
+
 		while(items.length) {
 			items[0].remove();
 		}
+	}
+
+	function clearFileInput() {
+		$control = $('#code-input');
+		$control.replaceWith($control = $control.clone(true));
+		$('.code-submission').attr("disabled", "disabled");
+		$('#code-input').parent().find('span')[0].innerHTML = "Select files";
+
 	}
 
 	$('input[type=file]').bootstrapFileInput();
@@ -42,16 +49,21 @@ $(document).ready(function() {
 		$('.navigation').hide();
 	}
 
+
 	$('.closeBtn').on("click", function() {
+		$debugButton = $(".debugButton");
+		$debugButton.css('outline', 0);
 		closeFeedback();
+
 	});
 
-	$('#code-input').on("change", function(){ 
- 		updateForm(); 
+	$('#code-input').on("change", function(){
+		updateForm();
+		
 	});
 
 	updateForm();
-
+	
     $('#upload-file-btn').click(function() {
         var form_data = new FormData($('#upload-file')[0]);
         $.ajax({
@@ -64,22 +76,37 @@ $(document).ready(function() {
             async: false,
             success: function(data) {
                 console.log('Success!');
-				$.each(
-					data.errors,
-					function(i,v) {
-						if(!v.search("Grading ")) {
-							$("#errorlist").append("<li><h4>" + v + "</h4></li>") ;
-						} else {
-							$("#errorlist").append("<li class='message-error'>" + v + "</li>");
+				
+                for(var i = 0; i < data.errors.length; ++i) {
+					if(data.errors[i + 1] && !data.errors[i].search("Grading ") && !data.errors[i + 1].search("Grading ") ) {
+						$("#errorlist").append("<li><h4>" + data.errors[i] + "</h4></li>");
+						$("#errorlist").append("<li class='message-success'>No errors have been found! :)</li>");
+					} else if(!data.errors[i + 1] && !data.errors[i].search("Grading ")) {
+						$("#errorlist").append("<li><h4>" + data.errors[i] + "</h4></li>");
+						$("#errorlist").append("<li class='message-success'>No errors have been found! :)</li>");
+					} else if(!data.errors[i].search("Grading ")) {
+						$("#errorlist").append("<li><h4>" + data.errors[i] + "</h4></li>");
+					} else {
+						$("#errorlist").append("<li class='message-error'>" + data.errors[i] + "</li>");
 					}
-					console.log(v);
-					}
-				);
+				}
+				// $.each(
+				// 	data.errors,
+				// 	function(i,v) {
+				// 		if(!v.search("Grading ")) {
+				// 			$("#errorlist").append("<li><h4>" + v + "</h4></li>") ;
+				// 		} else {
+				// 			$("#errorlist").append("<li class='message-error'>" + v + "</li>");
+				// 	}
+				// 	console.log(v);
+				// 	}
+				// );
                 if (data.errors.length === 1)
                 {
                 	// no errors
-                	$("#errorlist").append("<li class='message-success'>No errors have been found! :)</li>");
+					$("#errorlist").append("<li class='message-success'>No errors have been found! :)</li>");
                 }
+
 			},
 		});
 	});
