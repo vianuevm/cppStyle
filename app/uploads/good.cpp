d679cef99fa2863164ff8d46a3b662de35a09563
Billy Wolfington <man_of_magik903@yahoo.com>
2015-03-03 02:05:49 -0500

    Added app/uploads again

---
+++ app/uploads/good.cpp
@@ -0,0 +1,37 @@
+/*
+ * good.cpp
+ *
+ * First Last
+ * uniqname@umich.edu
+ *
+ * A sample file for EECS 183 Style Grader.
+ *
+ * Demonstrates good style.
+ */
+
+#include <iostream>
+using namespace std;
+
+/**
+ * Requires: Nothing.
+ * Modifies: stdout.
+ * Effects:  Greets the world.
+ */
+void greet(void);
+
+int main(int argc, const char * argv[])
+{
+    // greet the user
+    greet();
+    cout << "bye.\'" << '*' << '\\' << '\"' << '\'*' << '-' << '+' << endl;
+    int x;
+    cin >> x;
+
+    return 0;
+}
+
+void greet(void)
+{
+    cout << "o hai world!" << endl;
+    cout << "o hai world!" << endl;
+}
