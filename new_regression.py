f75a715b869c42629af02c24ade4d7a6c97f57d6
Billy Wolfington <man_of_magik903@yahoo.com>
2015-03-02 21:16:46 -0500

    Refactored new regression test

--- new_regression.py
+++ new_regression.py
@@ -6,10 +6,10 @@ import unittest
 
 class RegressionTesting(unittest.TestCase):
     @load_code_segment('logical_AND_OR_spacing_bad.cpp')
-    def test_bad_logical_spacing(self): self.assertTrue(self.rubric.error_types['OPERATOR_SPACING'] == 3)
+    def test_bad_logical_spacing(self): self.assertEqual(3, self.rubric.error_types['OPERATOR_SPACING'])
     @load_code_segment('logical_AND_OR_spacing_good.cpp')
-    def test_good_logical_spacing(self): self.assertTrue(self.rubric.error_types['OPERATOR_SPACING'] == 0)
+    def test_good_logical_spacing(self): self.assertTrue(0, self.rubric.error_types['OPERATOR_SPACING'])
     @load_code_segment('operator_spacing_bad.cpp')
-    def test_bad_operator_spacing(self): self.assertTrue(self.rubric.error_types['OPERATOR_SPACING'] == 20)
+    def test_bad_operator_spacing(self): self.assertTrue(20, self.rubric.error_types['OPERATOR_SPACING'])
     @load_code_segment('operator_spacing_good.cpp')
-    def test_good_operator_spacing(self): self.assertTrue(self.rubric.error_types['OPERATOR_SPACING'] == 2)
+    def test_good_operator_spacing(self): self.assertTrue(2, self.rubric.error_types['OPERATOR_SPACING'])
